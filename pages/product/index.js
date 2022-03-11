873666d395ca61dba8f94404fea0c43c3be3fcdd
Simon-Olivier Desautels <64502888+Simon-OlivierDesautels@users.noreply.github.com>
2022-03-11 07:30:31 -0500

    Video 10

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from "next/link";
+
 
 function ProductsList() {
   return (
       <ul>
-          <li>P1</li>
+        <Link href='/'>Home</Link>
+          <li><Link href='/product/1'> P1</Link></li>
           <li>P2</li>
           <li>P3</li>
       </ul>
