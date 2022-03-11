b1b6f8e21a53fb0e0b8acd5ac1d3dc520cb29bca
Simon-Olivier Desautels <64502888+Simon-OlivierDesautels@users.noreply.github.com>
2022-03-11 07:51:20 -0500

    Video 11

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.push('/product')
+  };
+
   return (
     <div>
       <h1>Home page </h1>
-      <Link href="/product">
-        Products
-      </Link>
+      <Link href="/product">Products</Link>
+
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 }
